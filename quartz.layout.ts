181609d080271f1da1ae9340e910398e3fdf8be1
Sabine Harnau <sabineharnau@macbook-air-2.lan>
2024-03-20 09:38:05 +0100

    Quartz sync: Mar 20, 2024, 9:38 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -7,7 +7,7 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
+      From Scratch Communications: "https://from-scratch.net/de/",
       "Discord Community": "https://discord.gg/cRFFHYye7t",
     },
   }),
